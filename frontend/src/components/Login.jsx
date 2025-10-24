dfecf786457afc29234b68d65262c7d4f3c56364
Elizaveta696 <89115037724L@gmail.com>
2025-10-24 20:59:39 +0300

    bug: fixed hardcoded url

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -7,7 +7,7 @@ export default function Login({ setToken, setPage }) {
 
   const handleLogin = async (e) => {
     e.preventDefault();
-    const res = await fetch("http://localhost:3000/api/auth/login", {
+    const res = await fetch(`${import.meta.env.VITE_API_URL}/api/auth/login`, {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ username, password }),
