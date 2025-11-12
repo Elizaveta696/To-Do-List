c64e5b54f56c55fe9420cccbdca4f1ca098fb478
AlicjaKo <alicjakosak@gmail.com>
2025-11-12 20:58:10 +0200

    build: security

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -3,10 +3,15 @@ import { Sequelize } from "sequelize";
 
 config();
 
-export const sequelize = new Sequelize(process.env.POSTGRES_DB, process.env.POSTGRES_USER, process.env.POSTGRES_PASSWORD, {
-    host: process.env.POSTGRES_HOST || 'db',
-    dialect: 'postgres',
-});
+export const sequelize = new Sequelize(
+	process.env.POSTGRES_DB,
+	process.env.POSTGRES_USER,
+	process.env.POSTGRES_PASSWORD,
+	{
+		host: process.env.POSTGRES_HOST || "db",
+		dialect: "postgres",
+	},
+);
 
 export const connectDB = async () => {
 	try {
